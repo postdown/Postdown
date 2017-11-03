34db75a83dc1b008fa933b13e6e5cfe20ccf1bdc
matthew <flame.xy@foxmail.com>
2017-11-03 17:08:24 +0800

    增加命令行参数支持

--- postdown/cmdline.py
+++ postdown/cmdline.py
@@ -4,14 +4,27 @@ from .parser import parse
 
 
 def execute():
-    in_file = sys.argv[1]
+    import argparse
 
-    if len(sys.argv) > 2:
-        out_file = sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--zh", action="store_true", help="生成中文文档")
+    parser.add_argument("json", type=str, help="Postman JSON文件")
+    parser.add_argument("markdown", type=str, help="生成的文档", nargs='?')
+
+    args = parser.parse_args()
+
+    in_file = args.json
+
+    if args.markdown:
+        out_file = args.markdown
     else:
         out_file = splitext(basename(in_file))[0] + '.md'
 
-    parse(in_file, out_file)
+    if args.zh:
+        parse_zh(in_file, out_file)
+    else:
+        parse(in_file, out_file)
+
 
 
 if __name__ == '__main__':
